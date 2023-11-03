15c9c723da9591ba0f7a91147036ada40709cb1a
allanstar-byte <allanmutisya263@gmail.com>
2023-11-03 14:00:49 +0300

    Setting up the API

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from .views import MovieViewSet, RatingViewSet
+
+router = routers.DefaultRouter()
+
+router.register('movies', MovieViewSet)
+router.register('raings', RatingViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
